6bea0be5925008661adff0d8d2c1846e732e96b3
Bycclin <bylin@stanford.edu>
2025-07-28 21:08:44 -0700

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,61 +1,50 @@
 #ifndef PARSER_H
 #define PARSER_H
-
 #include <string>
 #include <vector>
 #include "lexer.h"
-
 struct ASTNode {
     std::string type;
     std::string value;
     std::vector<ASTNode> children;
-    
+    Token token_ref;
     ASTNode();
-    ASTNode(const std::string& type, const std::vector<ASTNode>& children = {});
-    ASTNode(const std::string& type, const std::string& value, const std::vector<ASTNode>& children = {});
+    ASTNode(const std::string& type, const Token& ref_tok, const std::vector<ASTNode>& children = {});
+    ASTNode(const std::string& type, const std::string& value, const Token& ref_tok, const std::vector<ASTNode>& children = {});
     void print(size_t level = 0) const;
 };
-
 class Parser {
 public:
     explicit Parser(const std::vector<Token>& tokens);
     ASTNode parse();
-
 private:
-    std::vector<Token> tokens;
+    std::vector<Token> tokens_list;
     size_t position;
-    
+    Token currentToken() const;
+    Token consumeToken();
+    Token consumeToken(TokenType expectedType);
+    void expectToken(TokenType expectedType);
+    bool matchToken(TokenType type);
+    void expectToken(TokenType expectedType, const std::string& value);
+    bool matchToken(TokenType type, const std::string& value);
     ASTNode parseProgram();
     ASTNode parseStatement();
-    ASTNode parseSuite(); // New: Parse a block (suite) of statements.
-
-    // Control flow constructs.
-    ASTNode parseIf();
-    ASTNode parseWhile();
-    ASTNode parseFor();       // For-loop statement.
-    ASTNode parseWith();      // With statement support.
-
-    ASTNode parseImport();
-    ASTNode parseFromImport();
-    ASTNode parseFunctionCall();
-    ASTNode parseArgument();
-    
-    // Literal parsing.
-    ASTNode parseListLiteral();   // List literal support.
-    
+    ASTNode parseSimpleStatement();
+    ASTNode parseSmallStatement();
+    ASTNode parseCompoundStatement();
+    ASTNode parseSuite();
+    ASTNode parseIfStatement();
+    ASTNode parseWhileStatement();
     ASTNode parseFunctionDef();
-    ASTNode parseReturn();
-    ASTNode parseYield();
-    ASTNode parseClass();
-    ASTNode parseLambda();
-
-    Token getNextToken();
-    void skipUnsupportedStatement();
-    std::string currentTokenLocation() const;
-
-    // Helper functions for enhanced error messages.
-    std::string getLineSnippet(const Token &tk);
+    ASTNode parseReturnStatement();
+    ASTNode parseExpressionStatement();
+    ASTNode parseAssignmentStatement();
+    ASTNode parseBreakStatement();
+    ASTNode parseArgument();
+    ASTNode parseFunctionCall();
+    ASTNode parsePrimary();
+    ASTNode parseComparison();
     std::string formatError(const std::string &msg, const Token &tk);
+    std::string getLineSnippet(const Token &tk);
 };
-
-#endif // PARSER_H
+#endif
