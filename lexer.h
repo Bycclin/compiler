d0f5240075542bb4c199f5ac03b19ba82a1c05e8
Bycclin <bylin@stanford.edu>
2025-07-28 21:08:13 -0700

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -1,50 +1,48 @@
 #ifndef LEXER_H
 #define LEXER_H
-
 #include <string>
 #include <vector>
-#include <iosfwd>  // Use forward declaration for I/O streams instead of <iostream>
-
+#include <iostream>
+#include <stack>
 enum class TokenType {
     KEYWORD,
-    BUILTIN, // New token type for built‑in functions.
+    BUILTIN,
     IDENTIFIER,
     NUMBER,
     STRING,
     OPERATOR,
     PUNCTUATION,
     IMPORT,
-    END
+    NEWLINE,
+    INDENT,
+    DEDENT,
+    END,
+    UNKNOWN
 };
-
 struct Token {
     TokenType type;
     std::string value;
     int line;
     int column;
-    std::string filename; // Field for filename
-
+    std::string filename;
     Token(TokenType type, const std::string& value, int line = 0, int column = 0, const std::string& filename = "")
         : type(type), value(value), line(line), column(column), filename(filename) {}
 };
-
 std::ostream& operator<<(std::ostream& os, const TokenType& type);
-
 class Lexer {
 public:
-    // Accept an optional filename.
     explicit Lexer(const std::string& source, const std::string& filename = "");
     std::vector<Token> tokenize();
-
 private:
     std::string source;
     size_t position;
     int line;
     int column;
-    std::string filename; // Store filename
+    std::string filename;
     std::vector<std::string> keywords;
-
-    void handleWhitespace(char current);
+    std::vector<int> indent_stack;
+    bool at_line_start;
+    int paren_level;
     Token handleIdentifier();
     Token handleNumber();
     Token handleString(char quote);
@@ -54,9 +52,6 @@ private:
     bool isOperator(char current);
     bool isPunctuation(char current);
     Token handlePunctuation();
-
-    // Helper to get the content of the current line for error messages.
     std::string getLineSnippet(int lineNumber);
 };
-
-#endif // LEXER_H
+#endif
